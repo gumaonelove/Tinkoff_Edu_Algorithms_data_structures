a55303d22c8c4bf6fd06acc66074614b89f68d06
gumaonelove <68272600+gumaonelove@users.noreply.github.com>
2023-02-08 10:40:33 +0300

    Решение второй задачи из контеста по бинарному поиску (приближенный бинарный поиск)

--- binary_search/B.py
+++ binary_search/B.py
@@ -4,29 +4,35 @@
 Если есть несколько значений с минимальной разницей по модулю, надо вывести минимальное.
 '''
 
+# Алгоритм приближенного бинарного поиска
+
 n, k = map(int, input().split())
-N = list(map(int, input().split()))
-K = list(map(int, input().split()))
+N = list(map(int, input().split()))[:n]
+K = list(map(int, input().split()))[:k]
 
 
-def binary_search(key, last_key):
+def left_binary_search(key):
     start_element = 0
     end_element = n - 1
 
-    while start_element <= end_element:
-        middle_element = (start_element + end_element) // 2
+    if key >= N[-1]:
+        return N[-1]
+    elif key <= N[0]:
+        return N[0]
 
-        if N[middle_element] == key:
-            return key
-        elif N[middle_element] < key:
-            start_element = middle_element + 1
+    while start_element < end_element:
+        middle = (start_element + end_element) // 2
+        if N[middle] < key:
+            start_element = middle + 1
         else:
-            end_element = middle_element - 1
-    if last_key != key:
-        return binary_search(key + 1, key)
+            end_element = middle
+
+    if start_element > 0 and N[start_element] != key and abs(N[start_element - 1] - key) <= abs(N[start_element] - key):
+        return N[start_element - 1]
     else:
-        return binary_search(key - 1, key)
+        return N[start_element]
 
 
 for k_i in K:
-    print(binary_search(k_i, k_i))
+    left = left_binary_search(k_i)
+    print(left)
