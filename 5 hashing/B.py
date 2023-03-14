29883061083e79b8c72944b6af8a98c81b0ab7e4
gumaonelove <68272600+gumaonelove@users.noreply.github.com>
2023-03-14 17:33:18 +0300

    5.D by Manacher

--- 5 hashing/B.py
+++ 5 hashing/B.py
@@ -7,15 +7,15 @@ k = 31
 mod = 10 ** 9 + 7
 
 for i in range(1, s_l + 1):
-	p[i] = (111 * p[i - 1] * k) % mod;
+    p[i] = (111 * p[i - 1] * k) % mod
 
 hs = [ord(s[0])] * (s_l + 1)
 for i in range(1, s_l):
-    hs[i] = (hs[i - 1] + p[i] * ord(s[i])) % mod;
+    hs[i] = (hs[i - 1] + p[i] * ord(s[i])) % mod
 
 ht = ord(t[0])
 for i in range(1, t_l):
-	ht += (p[i] * ord(t[i])) % mod;
+    ht += (p[i] * ord(t[i])) % mod
 
 
 def get_hash(h: list, l: int, r: int):
@@ -25,10 +25,11 @@ def get_hash(h: list, l: int, r: int):
         result -= h[l - 1]
     return result % mod
 
+
 answer = []
- 
+
 for i in range(0, s_l - t_l + 1):
     if get_hash(hs, i, i + t_l - 1) == (ht * p[i]) % mod:
         answer.append(i)
-        
+
 print(*answer)
